c9b69d9bd511449446b73054ad10427797e7c06f
roeiu100 <119501308+roeiu100@users.noreply.github.com>
2025-12-23 16:10:23 +0200

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,21 +1,26 @@
-from pyspark import SparkContext
 import sys
+from pyspark import SparkConf, SparkContext
 
-if __name__ == "__main__":
-    sc = SparkContext("local", "Word Count")
-    sc.setLogLevel("ERROR")
-    
+def main():
+    conf = SparkConf().setAppName("DistinctCountCache").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
     if len(sys.argv) < 2:
-        print("Usage: word_count.py <file>")
+        print("Usage: program.py <input_file>")
         sys.exit(1)
 
-    lines = sc.textFile(sys.argv[1])
-    counts = lines.flatMap(lambda line: line.split(" ")) \
-                  .map(lambda word: (word, 1)) \
-                  .reduceByKey(lambda a, b: a + b)
-                  
-    output = counts.take(10)
-    print("-------------------------------------------")
-    for (word, count) in output:
-        print("%s: %i" % (word, count))
-    print("-------------------------------------------")
+    text_file = sc.textFile(sys.argv[1]) 
+    text_file.cache()
+
+    distinct_count = text_file.flatMap(lambda line: line.split(" ")) \
+                              .distinct() \
+                              .count()
+
+    print("--------------------------------------------")
+    print("Total distinct words: " + str(distinct_count))
+    print("--------------------------------------------")
+
+    sc.stop()
+
+if __name__ == "__main__":
+    main()
