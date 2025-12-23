8649c042d20aa1ead1325d8d156dde58b84258e3
roeiu100 <119501308+roeiu100@users.noreply.github.com>
2025-12-23 16:01:07 +0200

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,6 +1,25 @@
+from pyspark import SparkContext
+import sys
 
-counts = text_file.flatMap(lambda line: line.split(" ")) \
-              .repartition(5) \
-              .map(lambda word: (word, 1)) \
-              .reduceByKey(lambda a, b: a + b) \
-              .filter(lambda x: len(x[0])>5)
+if __name__ == "__main__":
+    sc = SparkContext("local", "Word Count")
+    sc.setLogLevel("ERROR")
+    
+    if len(sys.argv) < 2:
+        print("Usage: word_count.py <file>")
+        sys.exit(1)
+
+    lines = sc.textFile(sys.argv[1])
+    
+    counts = lines.flatMap(lambda line: line.split(" ")) \
+                  .repartition(5) \
+                  .map(lambda word: (word, 1)) \
+                  .reduceByKey(lambda a, b: a + b) \
+                  .filter(lambda x: len(x[0]) > 5)
+                  
+    output = counts.take(10)
+    
+    print("-------------------------------------------")
+    for (word, count) in output:
+        print("%s: %i" % (word, count))
+    print("-------------------------------------------")
